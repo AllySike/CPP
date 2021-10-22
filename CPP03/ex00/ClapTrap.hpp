df2713b9a553d0ab787261e2bfc8279221cba437
AllySike <mn1111@mail.ru>
2021-10-22 18:09:04 +0300

    CPP03/ex00

---
+++ CPP03/ex00/ClapTrap.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kgale <kgale@student.21-school.ru>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/22 13:43:27 by kgale             #+#    #+#             */
+/*   Updated: 2021/10/22 13:43:27 by kgale            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+#include "ClapTrap.hpp"
+#include <string>
+#include <iostream>
+
+class ClapTrap {
+	public:
+		ClapTrap();
+		ClapTrap(std::string name);
+		~ClapTrap();
+		ClapTrap(const ClapTrap &clapTrap);
+		ClapTrap& operator = (const ClapTrap& clapTrap);
+		void attack(const std::string& target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+	private:
+		std::string name;
+		int hitpoints;
+		int energy;
+		int damage;
+};
+
+#endif
--- CPP03/ex00/main.cpp
+++ CPP03/ex00/main.cpp
@@ -1,6 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kgale <kgale@student.21-school.ru>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/22 13:44:46 by kgale             #+#    #+#             */
+/*   Updated: 2021/10/22 13:44:46 by kgale            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ClapTrap.hpp"
 
 int main()
 {
-	return (0);
+	ClapTrap clap1("Clap1");
+	ClapTrap clap02("Clap2");
+	ClapTrap clap2(clap02);
+	ClapTrap clap03("Clap3");
+	ClapTrap clap3 = clap03;
+	std::cout << std::endl;
+	clap1.attack("who");
+	clap1.takeDamage(12);
+	clap1.beRepaired(1);
+	clap3.beRepaired(4);
+	clap2.takeDamage(6);
+	clap3.beRepaired(8);
+
+	std::cout << std::endl;
 }
