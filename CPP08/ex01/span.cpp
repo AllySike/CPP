8cff4a8659b36b3b198c17a4002eb7e916ebdb7b
AllySike <mn1111@mail.ru>
2021-11-12 16:40:20 +0300

    CPP08/ex01

--- CPP08/ex01/span.cpp
+++ CPP08/ex01/span.cpp
@@ -11,3 +11,93 @@
 /* ************************************************************************** */
 
 #include "span.hpp"
+
+Span::Span(): _size(0){}
+
+Span::Span(unsigned int n): _size(n), _arr(0){}
+
+Span::~Span(){}
+
+Span::Span(const Span &src):_size(src._size), _arr(src._arr){}
+
+Span& Span::operator=(const Span &src)
+{
+	this->_arr = src._arr;
+	this->_size = src._size;
+	return (*this);
+}
+
+void Span::addNumber(int n)
+{
+	if (this->_arr.size() == this->_size)
+		throw Span::TooManyNumbers();
+	this->_arr.push_back(n);
+}
+
+void Span::addNumberByIterator(std::vector <int>::iterator begin,
+	std::vector <int>::iterator end)
+{
+	std::vector<int> tmp(begin, end);
+
+	if (tmp.size() > this->_size - this->_arr.size())
+		throw Span::TooManyNumbers();
+	copy(tmp.begin(), tmp.end(), std::back_inserter(this->_arr));
+}
+
+int Span::shortestSpan() const
+{
+	std::vector<int> sorted = this->_arr;
+	int span = 0;
+
+	if (this->_arr.size() < 2)
+		throw Span::NotEnoughNumbersToFindSpan();
+	sort(sorted.begin(), sorted.end());
+	span = *(sorted.begin() + 1) - *sorted.begin();
+	for (std::vector<int>::iterator iter = sorted.begin(); *iter < *sorted.end(); iter++)
+	{
+		if (*(iter + 1) - *iter < span)
+			span = *(iter + 1) - *iter;
+	}
+	return (span);
+}
+
+int Span::longestSpan() const
+{
+	std::vector<int> sorted = this->_arr;
+
+	if (this->_arr.size() < 2)
+		throw Span::NotEnoughNumbersToFindSpan();
+	sort(sorted.begin(), sorted.end());
+	return (*(sorted.end() - 1) - *sorted.begin());
+}
+
+int Span::getSize() const
+{
+	return (this->_arr.size());
+}
+
+int Span::getCapacity() const
+{
+	return (this->_size);
+}
+
+std::ostream& operator<<(std::ostream &ostr, const Span& instance)
+{
+	try
+	{
+		int	shortest = instance.shortestSpan();
+		int	longest = instance.longestSpan();
+
+		ostr << "Span of capacity " << instance.getCapacity() << " contains "
+			<< instance.getSize() << " numbers, shortest span is "
+			<< shortest << " and longest is " << longest;
+	}
+	catch(const std::exception& e)
+	{
+		(void)e;
+		ostr << "Span of capacity " << instance.getCapacity() << " contains "
+			<< instance.getSize() << " number";
+	}
+
+	return ostr;
+}
--- CPP08/ex01/span.hpp
+++ CPP08/ex01/span.hpp
@@ -13,5 +13,47 @@
 #ifndef SPAN_HPP
 # define SPAN_HPP
 
+# include <vector>
+# include <iostream>
+# include <algorithm>
+
+class Span
+{
+	public:
+		Span();
+		Span(unsigned int n);
+		~Span();
+		Span(const Span &src);
+		Span& operator=(const Span &src);
+
+		void addNumber(int n);
+		void addNumberByIterator(std::vector <int>::iterator begin,
+			std::vector <int>::iterator end);
+		int shortestSpan() const;
+		int longestSpan() const;
+		int getSize() const;
+		int getCapacity() const;
+		class TooManyNumbers: public std::exception
+		{
+			public:
+				virtual const char* what() const throw()
+				{
+					return ("Too many numbers");
+				}
+		};
+		class NotEnoughNumbersToFindSpan: public std::exception
+		{
+			public:
+				virtual const char* what() const throw()
+				{
+					return ("Not enough numbers to find span");
+				}
+		};
+	private:
+		unsigned int _size;
+		std::vector<int> _arr;
+};
+
+std::ostream& operator<<(std::ostream &ostr, const Span& instance);
 
 #endif
