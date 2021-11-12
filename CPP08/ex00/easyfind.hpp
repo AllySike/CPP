44a07cf83fa55360b2982f9fe5924367151d27e7
AllySike <mn1111@mail.ru>
2021-11-12 13:40:26 +0300

    CPP08/ex00

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -13,4 +13,12 @@
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
+# include <algorithm>
+
+template <typename T>
+typename T::iterator easyfind(T &arr, int n)
+{
+	return (std::find(arr.begin(), arr.end(), n));
+}
+
 #endif
--- CPP08/ex00/main.cpp
+++ CPP08/ex00/main.cpp
@@ -11,8 +11,37 @@
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
+#include <vector>
+#include <iostream>
 
-int main()
+template<typename T>
+void	displayIterator(T it, T end)
 {
-	return (0);
+	if (it != end)
+		std::cout << "iterator: " << static_cast<char>(*it) << std::endl;
+	else
+		std::cout << "no such element" << std::endl;
+}
+
+int	main( void )
+{
+	std::vector<int>			vect;
+	std::vector<int>::iterator	it;
+
+	for (int i = 1; i < 99; i += 2)
+		vect.push_back(i);
+	it = easyfind(vect, 53);
+	displayIterator(it, vect.end());
+	it = easyfind(vect, 62);
+	displayIterator(it, vect.end());
+
+	std::vector<char>			vector;
+	std::vector<char>::iterator	iterator;
+
+	for (int i = 97; i < 123; i++)
+		vect.push_back(i);
+	it = easyfind(vect, 'c');
+	displayIterator(it, vect.end());
+	it = easyfind(vect, 'F');
+	displayIterator(it, vect.end());
 }
